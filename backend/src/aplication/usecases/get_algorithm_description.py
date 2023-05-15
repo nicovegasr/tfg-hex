d916ec37b8d29e8a778fe77fdb14cfb62700c579
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-05-15 15:11:28 +0100

    typo

--- backend/src/aplication/usecases/get_algorithm_description.py
+++ backend/src/aplication/usecases/get_algorithm_description.py
@@ -4,11 +4,11 @@ from aplication.usecases.get_configuration_file import get_configuration_file
 def get_algorithm_description(configuration_file: dict) -> list:
     try:
         description = f'Descripcion: Para que el algoritmo funcione debe introducir {configuration_file["n_files"]} archivos. '
-        for i in range(int(configuration_file["n_files"])):
+        for file_number in range(int(configuration_file["n_files"])):
             nulls = "permitidos"
-            if configuration_file["allowed_nulls"][i] == 0:
+            if configuration_file["allowed_nulls"][file_number] == 0:
                 nulls = "prohibidos"
-            description += f'El fichero  numero {i+1}: debe tener {configuration_file["n_cols"][i]} columna(s). Los tipos deben ser: {configuration_file["cols_types"][i]} y los nulos estan {nulls}. '
+            description += f'El fichero  numero {file_number + 1}: debe tener {configuration_file["n_cols"][file_number]} columna(s). Los tipos deben ser: {configuration_file["cols_types"][file_number]} y los nulos estan {nulls}. '
             aditional_description = (
                 f'Descripcion adicional: {configuration_file["aditional_description"]}'
             )
